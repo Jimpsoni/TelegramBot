3dc89769db9196c5a785755929d507df3e2fac35
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2022-11-01 23:34:15 +0200

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,5 +1,15 @@
-"""
+import telebot
+import telebot.types
 
-This is the telegrambot file
+API_KEY = "5655827893:AAFWtirXSN7ayS9xq9UKgi2KnvbVzxYl0rc"
+BOT_NAME = "Programmerhumor_bot"
 
-"""
+bot = telebot.TeleBot(API_KEY)
+
+
+@bot.message_handler(commands=["greet"])
+def greet(message):
+    bot.send_message(message, "Hey!")
+
+
+# bot.polling()
